b3a04a3cdd5171a6b0a4d509a6dd1c5f85750977
Patrick Niyogitare <54368821+PatrickNiyogitare28@users.noreply.github.com>
2024-01-11 22:13:13 +0200

    Update firebase.js

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCd5NfSGl6MPvSJNIMSPm0F6JLbqPd1dZg",
-    authDomain: "codetyhub-firebase-demo.firebaseapp.com",
-    projectId: "codetyhub-firebase-demo",
-    storageBucket: "codetyhub-firebase-demo.appspot.com",
-    messagingSenderId: "1081889698733",
-    appId: "1:1081889698733:web:96fcedf4d88a44001a2724",
-    measurementId: "G-38YPB1RKMD"
+    apiKey: "***",
+    authDomain: "***",
+    projectId: "***",
+    storageBucket: "***",
+    messagingSenderId: "***",
+    appId: "***",
+    measurementId: "***"
   };
 
   const app = firebase.initializeApp(firebaseConfig);
