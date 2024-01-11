1c75a30611faa86254b986c518ed8242ce6ad42c
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2024-01-11 20:58:22 +0200

    Added auth

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,37 +1,43 @@
 const onSignup = () => {
-    const first_name = document.getElementById("firstName").value;
-    const last_name = document.getElementById("lastName").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    
-    auth.createUserWithEmailAndPassword(email, password).then(async (authUser) => {
-        console.log({first_name, last_name, email, created_at: new Date()})
-        db.collection("users").doc().set({first_name, last_name, email, created_at: new Date()})
-        .then(() => {
-            alert("User created successfully")
-            window.location.href="./login.html"
-        })
-        .catch((error) => {
-            console.log(error)
-            console.log("Profile registration Error")
-        })
-    })
-    .catch(error => {
-        console.log("error occurered", error)
-        alert("User signup error")
-    })
+   const first_name = document.getElementById("firstName").value; 
+   const last_name = document.getElementById("lastName").value; 
+   const email = document.getElementById("email").value; 
+   const password = document.getElementById("password").value; 
+
+   console.log({
+    first_name, last_name, email, password
+   })
+
+   auth.createUserWithEmailAndPassword(email, password)
+   .then((userAccount) => {
+     db.collection("users").doc().set({first_name, last_name, email, created_at: new Date()})
+     .then((userProfile) => {
+         alert("User registered successfully")
+         console.log(userProfile)
+         window.location.href = "./login.html";
+     })
+     .catch((error) => {
+        alert("User profile not registered")
+        console.log(error)
+     })
+   })
+   .catch((error) => {
+    alert("Error occurred");
+    console.log(error)
+   })
 }
 
 const onLogin = () => {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    
-    auth.signInWithEmailAndPassword(email, password).then(async (authUser) => {
-       alert("You logged In successfully")
-       console.log(authUser)
+    const email = document.getElementById("email").value; 
+    const password = document.getElementById("password").value; 
+
+    auth.signInWithEmailAndPassword(email, password)
+    .then((userSession) => {
+        alert("User logged In successfully")
+        console.log(userSession);
     })
-    .catch(error => {
-        console.log("error occurered", error)
+    .catch((error) => {
         alert("Invalid credentials")
+        console.log(error)
     })
 }
--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,14 +1,13 @@
-  const firebaseConfig = {
-    apiKey: "**",
-    authDomain: "**",
-    projectId: "**",
-    storageBucket: "**",
-    messagingSenderId: "**",
-    appId: "**",
-    measurementId: "**"
+const firebaseConfig = {
+    apiKey: "AIzaSyCd5NfSGl6MPvSJNIMSPm0F6JLbqPd1dZg",
+    authDomain: "codetyhub-firebase-demo.firebaseapp.com",
+    projectId: "codetyhub-firebase-demo",
+    storageBucket: "codetyhub-firebase-demo.appspot.com",
+    messagingSenderId: "1081889698733",
+    appId: "1:1081889698733:web:96fcedf4d88a44001a2724",
+    measurementId: "G-38YPB1RKMD"
   };
 
-  // Initialize Firebase
   const app = firebase.initializeApp(firebaseConfig);
   const auth = firebase.auth();
   const db = firebase.firestore();
